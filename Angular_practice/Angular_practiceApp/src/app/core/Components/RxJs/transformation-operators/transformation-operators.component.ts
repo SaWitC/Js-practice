53c47c2c8db1b46f86bdf0d6acd7b259550613d6
SaWitC <ozabello21@gmail.com>
2024-01-15 22:10:32 +0300

    Cleanup

--- Angular_practice/Angular_practiceApp/src/app/core/Components/RxJs/transformation-operators/transformation-operators.component.ts
+++ Angular_practice/Angular_practiceApp/src/app/core/Components/RxJs/transformation-operators/transformation-operators.component.ts
@@ -1,7 +1,5 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { ToastrService } from 'ngx-toastr';
-import { Observable, observable, of, map, from, mapTo, pluck, scan, expand, mergeMap, interval, buffer, fromEvent, takeUntil, Subject, Subscription, concatMap, take, switchMap, exhaustMap } from 'rxjs';
+import {of, map, mapTo, mergeMap, interval, takeUntil, Subject, concatMap, take, switchMap, exhaustMap } from 'rxjs';
 import { EventData } from 'src/app/core/models/EventsModel';
 import { SubscriptionData } from 'src/app/core/models/SubscriptionData.model';
 import { EventsService } from 'src/app/core/services/events-service.service';
@@ -16,7 +14,7 @@ export class TransformationOperatorsComponent implements OnDestroy {
 
   private destroy$ = new Subject<void>()
 
-  constructor(public subscriptionsServiceService:SubscriptionsServiceService,public eventsService:EventsService,private toastr: ToastrService) {
+  constructor(public subscriptionsServiceService:SubscriptionsServiceService,public eventsService:EventsService) {
   }
 
   private readonly page ="transformation-operators";
@@ -60,73 +58,79 @@ export class TransformationOperatorsComponent implements OnDestroy {
   .pipe(takeUntil(this.destroy$),switchMap(x=>interval(1000).pipe(take(x))))`
 
   rxJsMap() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+    this.eventsService.log(new EventData("blue","Add subscription with map",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(1, 2, 3, 4, 5).pipe(takeUntil(this.destroy$),map((x) => x * 2)).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(1, 2, 3, 4, 5).pipe(takeUntil(this.destroy$),map((x) => x * 2))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
 
   rxJsMapTo() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+    this.eventsService.log(new EventData("blue","Add subscription with mapTo",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(1, 2, 3, 4, 5).pipe(takeUntil(this.destroy$),mapTo('a')).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(1, 2, 3, 4, 5).pipe(takeUntil(this.destroy$),mapTo('a'))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
 
   rxJsConcatMap() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+    this.eventsService.log(new EventData("blue","Add subscription with ConcatMap",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),concatMap(x=>interval(1000).pipe(take(x)))).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),concatMap(x=>interval(1000).pipe(take(x))))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
 
-  rxJsSwirtchMap() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+  rxJsSwitchMap() {
+    this.eventsService.log(new EventData("blue","Add subscription with switchMap",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),switchMap(x=>interval(1000).pipe(take(x)))).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),switchMap(x=>interval(1000).pipe(take(x))))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
 
   rxJsExaustMap() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+    this.eventsService.log(new EventData("blue","Add subscription with exhaustMap",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),exhaustMap(x=>interval(1000).pipe(take(x)))).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),exhaustMap(x=>interval(1000).pipe(take(x))))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
 
   rxJsMergeMap() {
-    this.eventsService.log(new EventData("blue","Add subscription",""))
+    this.eventsService.log(new EventData("blue","Add subscription with mergeMap",""))
 
-    this.onMsgClicked.emit("of(1, 2, 3, 4, 5).pipe(map((x) => x * 2))");
-    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),mergeMap(x=>interval(1000).pipe(take(x)))).subscribe(
-      res => this.toastr.info("observer got value " + res, "info"),
-      err => this.toastr.error(err),
-      () => { this.toastr.success("complete"); }
+    const subscrib = of(2, 1).pipe(takeUntil(this.destroy$),mergeMap(x=>interval(1000).pipe(take(x))))
+    .subscribe(
+      res => this.eventsService.logInfo("observer got value " + res),
+      err => this.eventsService.logSuccseed(err),
+      () => {
+        this.eventsService.logSuccseed("complete")}
     )
     this.subscriptionsServiceService.addSubscription(new SubscriptionData("of with map",subscrib,this.page))
   }
--- Angular_practice/Angular_practiceApp/src/app/core/services/events-service.service.ts
+++ Angular_practice/Angular_practiceApp/src/app/core/services/events-service.service.ts
@@ -18,5 +18,25 @@ export class EventsService {
     this.events.push(sub);
     return this.getSubscriptionsSubject$.next(this.events);
   }
+  public logSuccseed(message:string){
+    this.events.push(new EventData("#3CB371",message,""));
+    return this.getSubscriptionsSubject$.next(this.events);
+  }
+  public logError(message:string){
+    this.events.push(new EventData("#FF4500",message,""));
+    return this.getSubscriptionsSubject$.next(this.events);
+  }
+  public logWarning(message:string){
+    this.events.push(new EventData("#FFA500",message,""));
+    return this.getSubscriptionsSubject$.next(this.events);
+  }
+  public logInfo(message:string){
+    this.events.push(new EventData("RoyalBlue",message,""));
+    return this.getSubscriptionsSubject$.next(this.events);
+  }
+  public logUsubscribe(message:string){
+    this.events.push(new EventData("RoyalBlue",message,""));
+    return this.getSubscriptionsSubject$.next(this.events);
+  }
   constructor() { }
 }
